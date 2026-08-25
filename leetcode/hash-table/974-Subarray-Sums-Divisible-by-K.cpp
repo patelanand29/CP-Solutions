b8095b7dd0059f1f6ee9fc2c7fe07d83be2b75fc
Anand Patel <anandpatelofficial0@gmail.com>
2026-08-25 15:34:22 +0530

    Add Subarray-Sums-Divisible-by-K (hash-table)

---
+++ leetcode/hash-table/974-Subarray-Sums-Divisible-by-K.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        
+        for(int i=1;i<nums.size();i++){
+            nums[i]+=nums[i-1];
+        }
+        for(int i=0;i<nums.size();i++){
+            nums[i]%=k;
+        }
+        int ans=0;
+        unordered_map<int,int>mp;
+        mp[0]=1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%k>=0){
+                ans+=mp[nums[i]%k];
+                mp[nums[i]]++;}
+            else{
+                ans+=mp[(nums[i]%k+k)%k];
+                mp[nums[i]%k+k]++;
+            } 
+            
+        }
+        return ans;
+    }
+};
