a5feb56864f4d06549a54ebe63ad5f318cab2115
Anand Patel <anandpatelofficial0@gmail.com>
2026-08-25 21:03:23 +0530

    Add Maximum-Number-of-Vowels-in-a-Substring-of-Given-Length (sliding-window)

--- leetcode/sliding-window/1456-Maximum-Number-of-Vowels-in-a-Substring-of-Given-Length.cpp
+++ leetcode/sliding-window/1456-Maximum-Number-of-Vowels-in-a-Substring-of-Given-Length.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int maxVowels(string s, int k) {
         unordered_map<char,int>mp;
-        int ans=0;
+        int ans=0,cnt=0;
         for(int i=0;i<s.size();i++){
             
             if(i<k-1){
-                if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u') mp[s[i]]++;
+                if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u') cnt++;
             }
             else if(i==k-1){
-                if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u') mp[s[i]]++;
-                ans=max(ans,mp['a']+mp['e']+mp['i']+mp['o']+mp['u']);
+                if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u') cnt++;
+                ans=max(ans,cnt);
             }
             else{
-                if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u') mp[s[i]]++;
-                if(s[i-k]=='a'||s[i-k]=='e'||s[i-k]=='i'||s[i-k]=='o'||s[i-k]=='u') mp[s[i-k]]--;
-                ans=max(ans,mp['a']+mp['e']+mp['i']+mp['o']+mp['u']);
+                if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u') cnt++;
+                if(s[i-k]=='a'||s[i-k]=='e'||s[i-k]=='i'||s[i-k]=='o'||s[i-k]=='u') cnt--;
+                ans=max(ans,cnt);
             }
         }
         return ans;
