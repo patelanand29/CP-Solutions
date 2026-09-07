b87acd62156949b475550e9ffcf0b1a837003b06
Anand Patel <anandpatelofficial0@gmail.com>
2026-09-08 00:44:32 +0530

    Add Саня и ромб (800)

---
+++ codeforces/800/1180A - Саня и ромб.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+ios_base :: sync_with_stdio(false);
+cin.tie(nullptr);
+cout.tie(nullptr);
+     ll n;
+     cin>>n;
+    cout<<2*n*n-2*n+1<<endl;
+
+return 0;}
