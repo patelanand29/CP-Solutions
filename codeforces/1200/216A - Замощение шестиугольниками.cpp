4444f1e01a27718194910e50cfd79b7698b743e4
Anand Patel <anandpatelofficial0@gmail.com>
2026-09-19 12:11:42 +0530

    Add Замощение шестиугольниками (1200)

---
+++ codeforces/1200/216A - Замощение шестиугольниками.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+ios_base :: sync_with_stdio(false);
+cin.tie(nullptr);
+cout.tie(nullptr);
+ll a,b,c;
+cin>>a>>b>>c;
+cout<<(b+a-1)*(c+a-1)- (a-1)*a<<endl;
+return 0;}
